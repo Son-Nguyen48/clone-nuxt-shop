fd030124001a4f976df1d8d114c643fc02110fcd
Nguyễn Thế Sơn <55500547+Son-Nguyen48@users.noreply.github.com>
2023-07-06 01:04:51 +0700

    fix api link

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,8 +77,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3004',
-    // baseURL: 'https://uthcq8-3000.csb.app',
+    // baseURL: 'http://localhost:3004',
+    baseURL: 'https://uthcq8-3000.csb.app',
     // baseURL: 'https://api-nuxt-shop.vercel.app',
   },
 
